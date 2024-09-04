57784e65a57e93dc0918967e47b8ee9283b7cf1e
Felipe Xavier <fd192168@gmail.com>
2024-09-04 17:26:09 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var nota1 = Number(prompt("Digite um número"));
+var nota2 = Number(prompt("Digite outro número"));
+var nota3 = Number(prompt("Digite mais um número"));
+var media = (nota1 + nota2 + nota3) / 3
+
+if (media > 8) {
+    alert('Parabéns pelo empenho');
+}else if(media >= 6 && media <= 8) {
+    alert('Boa chegou lá!');
+}else {
+    alert('Infezlimente foi reprovado!');
+}
